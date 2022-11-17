b7e7097c1b9b63e8a1d56da183c8daa3c9653a05
pratik.shinde_patchinfotech <pratik.shinde@patchinfotech.com>
2022-11-17 20:47:29 +0530

    added necessary javascript to count chars in textarea

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your code here
+const letterCount = document.getElementById('letterCount')
+
+function renderCount() {
+    const textArea = document.getElementById('evaluatedText').value
+    text = document.createTextNode(textArea.length)
+    letterCount.textContent = ""
+    letterCount.appendChild(text)
+}
